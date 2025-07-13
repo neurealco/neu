598a08b5a3041db971bd7c853d85f6c5d55d2f97
Neureal <neurealco@gmail.com>
2025-07-13 02:51:53 +0000

    app-0001-0002

---
+++ apps/frontend/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import node from "@astrojs/node"; // Importante para SSR
+
+export default defineConfig({
+  integrations: [react()],
+  output: "server", // Habilitar SSR
+  adapter: node({
+    mode: "standalone",
+  }),
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+  },
+});
