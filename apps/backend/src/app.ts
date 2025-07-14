f170d3b7b69b3cd84ab9a395a90d3e648dd0b29f
Neureal <neurealco@gmail.com>
2025-07-14 21:58:28 +0000

    app-m2

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,24 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import config from './config';
-import routes from './routes';
-import errorHandler from './middleware/error.middleware';
-import { rateLimiter } from './utils/rateLimit.util';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from "./config";
+import routes from "./routes";
+import { errorHandler } from "./middleware/error.middleware";
+import { apiRateLimiter } from "./utils/rateLimit.util";
 
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: config.SITE_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: config.SITE_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(rateLimiter);
 
 // Routes
-app.use('/api', routes);
+app.use("/api", routes);
 
 // Error handling
 app.use(errorHandler);
