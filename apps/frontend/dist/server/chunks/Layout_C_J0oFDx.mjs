07b7aacc93a757167f88fa9f3c3f5ddf1053f234
neurealco <neurealco@gmail.com>
2025-07-28 19:32:39 +0000

    akssj

--- apps/frontend/dist/server/chunks/Layout_C_J0oFDx.mjs
+++
@@ -1,7 +0,0 @@
-import { c as createComponent, r as renderTemplate } from './astro/server_LK4p-fTz.mjs';
-
-const $$Layout = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate``;
-}, "/workspaces/neu/apps/frontend/src/components/layout/Layout.astro", void 0);
-
-export { $$Layout as $ };
