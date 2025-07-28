fcc383c05444582fe7c74ce46757c10503cb3a44
neurealco <neurealco@gmail.com>
2025-07-28 21:03:27 +0000

    hahh

--- apps/frontend/dist/server/entry.mjs
+++ apps/frontend/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Up0aijen.mjs';
-import { manifest } from './manifest_DGpSK2Cr.mjs';
+import { manifest } from './manifest_D_Sz-9Ut.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/dashboard/ai-assistant.astro.mjs');
